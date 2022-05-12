1be69fb32dfc7fa95a1a4c1cdfde9b1e9462fed4
JISOO2206 <900522z@gmail.com>
2022-05-12 17:50:34 +0900

    MinMax Test 추가

--- src/array/MinMax.java
+++ src/array/MinMax.java
@@ -1,7 +1,26 @@
 package array;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class MinMax {
 
+    public static void main(String[] args) throws IOException {
 
+        StringTokenizer st;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int ContainerNum = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> numArray = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        numArray.add(Integer.parseInt(st.nextToken()));
+
+
+    }
 
 }
